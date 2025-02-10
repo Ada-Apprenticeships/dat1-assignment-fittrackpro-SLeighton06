5b5736e02c3bc20c069322520727586132ef1fc5
SLeighton06 <seth.leighton@ada.ac.uk>
2025-02-10 20:21:50 +0000

    6.2 done

--- src/attendance_tracking.sql
+++ src/attendance_tracking.sql
@@ -10,10 +10,13 @@ PRAGMA foreign_keys = ON;
 -- 1. Record a member's gym visit
 -- TODO: Write a query to record a member's gym visit
 INSERT INTO attendance (member_id, location_id, check_in_time)
-VALUES (7, 1, DATETIME('now'));
+VALUES (7, 1, datetime('now'));
 
 -- 2. Retrieve a member's attendance history
 -- TODO: Write a query to retrieve a member's attendance history
+SELECT substr(check_in_time, 1, 10) AS visit_date, substr(check_in_time, 12, 8) AS check_in_time, substr(check_out_time, 12, 8) AS check_out_time
+FROM attendance
+WHERE member_id = 5;
 
 -- 3. Find the busiest day of the week based on gym visits
 -- TODO: Write a query to find the busiest day of the week based on gym visits
