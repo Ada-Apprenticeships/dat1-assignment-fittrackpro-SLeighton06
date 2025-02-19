aaa4e098b2b45ba2e3149db3d7720102412f79f9
SLeighton06 <seth.leighton@ada.ac.uk>
2025-02-19 11:46:26 +0000

    checks updated

--- src/schema.sql
+++ src/schema.sql
@@ -36,7 +36,7 @@ CREATE TABLE locations (
     address VARCHAR(32) NOT NULL,
     phone_number VARCHAR(32) NOT NULL,
     email VARCHAR(32) NOT NULL CHECK(email LIKE '%@%'),
-    opening_hours VARCHAR(11) NOT NULL CHECK(opening_hours REGEXP '^[0-2]?[0-9]:[0-5][0-9]-[0-2]?[0-9]:[0-5][0-9]$')
+    opening_hours VARCHAR(11) NOT NULL CHECK(opening_hours LIKE '%:%-%:%')
 );
 
 CREATE TABLE members (
@@ -45,8 +45,8 @@ CREATE TABLE members (
     last_name VARCHAR(32) NOT NULL,
     email VARCHAR(32) NOT NULL CHECK(email LIKE '%@%'),
     phone_number VARCHAR(32) NOT NULL,
-    date_of_birth DATE NOT NULL CHECK(date_of_birth REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
-    join_date DATE NOT NULL CHECK(join_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
+    date_of_birth DATE NOT NULL CHECK(date_of_birth GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
+    join_date DATE NOT NULL CHECK(join_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
     emergency_contact_name VARCHAR(32) NOT NULL,
     emergency_contact_phone VARCHAR(32) NOT NULL
 );
@@ -58,7 +58,7 @@ CREATE TABLE staff (
     email VARCHAR(32) NOT NULL CHECK(email LIKE '%@%'),
     phone_number VARCHAR(32) NOT NULL,
     position VARCHAR(16) NOT NULL CHECK(position IN ('Trainer','Manager','Receptionist','Maintenance')),
-    hire_date DATE NOT NULL CHECK(hire_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
+    hire_date DATE NOT NULL CHECK(hire_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
     location_id INTEGER NOT NULL,
     FOREIGN KEY (location_id) REFERENCES locations(location_id)
 );
@@ -67,9 +67,9 @@ CREATE TABLE equipment (
     equipment_id INTEGER PRIMARY KEY,
     name VARCHAR(32) NOT NULL,
     type VARCHAR(32) NOT NULL CHECK(type IN ('Cardio','Strength')),
-    purchase_date DATE NOT NULL CHECK(purchase_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
-    last_maintenance_date DATE NOT NULL CHECK(last_maintenance_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
-    next_maintenance_date DATE NOT NULL CHECK(next_maintenance_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
+    purchase_date DATE NOT NULL CHECK(purchase_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
+    last_maintenance_date DATE NOT NULL CHECK(last_maintenance_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
+    next_maintenance_date DATE NOT NULL CHECK(next_maintenance_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
     location_id INTEGER NOT NULL,
     FOREIGN KEY (location_id) REFERENCES locations(location_id)
 );
@@ -88,8 +88,8 @@ CREATE TABLE class_schedule (
     schedule_id INTEGER PRIMARY KEY,
     class_id INTEGER NOT NULL,
     staff_id INTEGER NOT NULL,
-    start_time DATETIME NOT NULL CHECK(start_time REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])\s([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$'),
-    end_time DATETIME NOT NULL CHECK(end_time REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])\s([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$'),
+    start_time DATETIME NOT NULL CHECK(start_time GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]'),
+    end_time DATETIME NOT NULL CHECK(end_time GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]'),
     FOREIGN KEY (class_id) REFERENCES classes(class_id),
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
 );
@@ -98,8 +98,8 @@ CREATE TABLE memberships (
     membership_id INTEGER PRIMARY KEY,
     member_id INTEGER NOT NULL,
     type VARCHAR(7) NOT NULL CHECK(type IN ('Basic', 'Premium')),
-    start_date DATE NOT NULL CHECK(start_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
-    end_date DATE NOT NULL CHECK(end_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
+    start_date DATE NOT NULL CHECK(start_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
+    end_date DATE NOT NULL CHECK(end_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
     status VARCHAR(8) NOT NULL CHECK(status IN ('Active','Inactive')),
     FOREIGN KEY (member_id) REFERENCES members(member_id)
 );
@@ -108,8 +108,8 @@ CREATE TABLE attendance (
     attendance_id INTEGER PRIMARY KEY,
     member_id INTEGER NOT NULL,
     location_id INTEGER NOT NULL,
-    check_in_time DATETIME NOT NULL CHECK(check_in_time REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])\s([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$'),
-    check_out_time DATETIME CHECK(check_out_time REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])\s([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$'),
+    check_in_time DATETIME NOT NULL CHECK(check_in_time GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]'),
+    check_out_time DATETIME CHECK(check_out_time GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]'),
     FOREIGN KEY (member_id) REFERENCES members(member_id),
     FOREIGN KEY (location_id) REFERENCES locations(location_id)
 );
@@ -127,7 +127,7 @@ CREATE TABLE payments (
     payment_id INTEGER PRIMARY KEY,
     member_id INTEGER NOT NULL,
     amount INTEGER NOT NULL,
-    payment_date DATETIME NOT NULL CHECK(payment_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])\s([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$'),
+    payment_date DATETIME NOT NULL CHECK(payment_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]'),
     payment_method VARCHAR(32) NOT NULL CHECK(payment_method IN ('Credit Card','Bank Transfer','PayPal','Cash')),
     payment_type VARCHAR(32) NOT NULL CHECK(payment_type IN ('Monthly membership fee','Day pass')),
     FOREIGN KEY (member_id) REFERENCES members(member_id)
@@ -137,9 +137,9 @@ CREATE TABLE personal_training_sessions (
     session_id INTEGER PRIMARY KEY,
     member_id INTEGER NOT NULL,
     staff_id INTEGER NOT NULL,
-    session_date DATE NOT NULL CHECK(session_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
-    start_time INTEGER NOT NULL,
-    end_time INTEGER NOT NULL,
+    session_date DATE NOT NULL CHECK(session_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
+    start_time INTEGER NOT NULL CHECK(start_time GLOB '[0-2][0-9]:[0-5][0-9]:[0-5][0-9]'),
+    end_time INTEGER NOT NULL CHECK(end_time GLOB '[0-2][0-9]:[0-5][0-9]:[0-5][0-9]'),
     notes VARCHAR(32) NOT NULL,
     FOREIGN KEY (member_id) REFERENCES members(member_id),
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
@@ -148,7 +148,7 @@ CREATE TABLE personal_training_sessions (
 CREATE TABLE member_health_metrics (
     metric_id INTEGER PRIMARY KEY,
     member_id INTEGER NOT NULL,
-    measurement_date DATE NOT NULL CHECK(measurement_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
+    measurement_date DATE NOT NULL CHECK(measurement_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
     weight INTEGER NOT NULL,
     body_fat_percentage INTEGER NOT NULL,
     muscle_mass INTEGER NOT NULL,
@@ -159,7 +159,7 @@ CREATE TABLE member_health_metrics (
 CREATE TABLE equipment_maintenance_log (
     log_id INTEGER PRIMARY KEY,
     equipment_id INTEGER NOT NULL,
-    maintenance_date DATE NOT NULL CHECK(maintenance_date REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0,1])$'),
+    maintenance_date DATE NOT NULL CHECK(maintenance_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]'),
     description VARCHAR(32) NOT NULL,
     staff_id INTEGER NOT NULL,
     FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id),
